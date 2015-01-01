a75726c9d7e676188150643fa98703f62281e908
Alice H <alice@hohlabs.in>
2015-01-01 15:26:09 +0530

    fiber

--- x86/main.cc
+++ x86/main.cc
@@ -3,6 +3,7 @@
 #include "labs/keyboard.h"
 #include "labs/shell.h"
 #include "labs/coroutine.h"
+#include "labs/fiber.h"
 
 
 
@@ -12,8 +13,19 @@ struct core_t{
   int           vgatext_height; //=25;
 
   shellstate_t  shell_state;
+
+  //for one coroutine
   coroutine_t   f_coro;
   f_t           f_locals;
+
+  //for one fiber
+  addr_t        main_stack;
+  addr_t        f_stack;
+  size_t        f_arraysize;
+  uint32_t      f_debug1;
+  uint8_t       f_array[4096] ALIGN(64);
+  uint32_t      f_debug2;
+
 
   renderstate_t render_state; //separate renderstate from shellstate
 
@@ -58,10 +70,16 @@ extern "C" void core_init(core_t& core){
   core.vgatext_base   = (addr_t)0xb8000;
   core.vgatext_width  = 80;
   core.vgatext_height = 25;
+  core.main_stack     = addr_t(0xfacebaad); //to tell you that their value is random
+  core.f_stack        = addr_t(0xfacebaad);
+  core.f_arraysize    = sizeof(core.f_array);
+  core.f_debug1       = 0xface600d; // for debug
+  core.f_debug2       = 0xface600d;
 
   lpc_kbd_initialize(&core.lpc_kbd,0x60);
 
   shell_init(core.shell_state);
+
   shell_render(core.shell_state, core.render_state);
 }
 
@@ -116,6 +134,9 @@ nokey:
 
   // execute shell for one time slot to do some computation based on coroutine, if required.
   shell_step_coroutine(core.shell_state, core.f_coro, core.f_locals);
+
+  // execute shell for one time slot to do some computation based on fiber, if required.
+  shell_step_fiber(core.shell_state, core.main_stack, core.f_stack, core.f_array, core.f_arraysize);
 
   // shellstate -> renderstate: compute render state from shell state
   shell_render(core.shell_state, rendertmp);
