3584c36e6f6c67b980f21d84d201f696ca93eede
Shivam Garg <gowri.cse.iitd.ac.in>
2015-04-14 00:06:04 +0530

    1.10 and 1.11 working

--- labs/ring3_preempt.h
+++ labs/ring3_preempt.h
@@ -9,8 +9,8 @@
 //
 //
 
-#define _ring3_preempt(_name, _f) \
-         asm volatile(                                   \
+#define _ring3_preempt(_name, _f)   \
+         __asm(                                   \
          "  .text                            \n\t"\
          " " STR(_name) ":                   \n\t"\
          "  pushl %edx                       \n\t"\
@@ -20,30 +20,36 @@
          "  popl  %eax                       \n\t"\
          "  popl  %ecx                       \n\t"\
          "  popl  %edx                       \n\t"\
-         "  pushl %%eax						 \n\t"\
-         "  movl %%gs:36, %%eax				 \n\t"\
-         "  movl %%edi , $0x400(%%eax)       \n\t"\
-         "  movl %%esi , $0x404(%%eax)       \n\t"\
-         "  movl %%ebp , $0x408(%%eax)       \n\t"\
-         "  movl %%ebx , $0x410(%%eax)       \n\t"\
-         "  movl %%edx , $0x414(%%eax)       \n\t"\
-         "  movl %%ecx , $0x418(%%eax)       \n\t"\
-         "  movl %%esp , $0x40c(%%eax)       \n\t"\
-         "  movl %%eip , $0x420(%%eax)       \n\t"\
-         "  movl %%eflags, $0x424(%%eax)     \n\t"\
-         "  fxsave 0x400(%%eax)              \n\t"\
-         "  popl %%ebx                       \n\t"\
-         "  movl %%ebx , $0x41c(%%eax)       \n\t"\
-         "                                   \n\t"\
+         "  pushl %eax						 \n\t"\
+         "  movl %gs:32, %eax				 \n\t"\
+         "  #movl (%eax), %eax				\n\t"\
+         "  movl %edi , 0x400(%eax)       \n\t"\
+         "  movl %esi , 0x404(%eax)       \n\t"\
+         "  movl %ebp , 0x408(%eax)       \n\t"\
+         "  movl %ebx , 0x410(%eax)       \n\t"\
+         "  movl %edx , 0x414(%eax)       \n\t"\
+         "  movl %ecx , 0x418(%eax)       \n\t"\
+         "  fxsave (%eax)              \n\t"\
+         "  popl %ebx                       \n\t"\
+         "  movl %ebx , 0x41c(%eax)       \n\t"\
+         "  popl %ebx						\n\t"\
+         "  popl %ebx						\n\t"\
+         "  popl %ebx						\n\t"\
+         "  movl %ebx, 0x420(%eax)			\n\t"\
+         "	popl %ebx						\n\t"\
+         "	popl %ebx						\n\t"\
+         "	movl %ebx, 0x424(%eax)			\n\t"\
+         "	popl %ebx						\n\t"\
+         "	movl %ebx, 0x40c(%eax)			\n\t"\
+         "  movl %gs:28, %ebx				\n\t"\
+         "	movl %ebx, %cr3					\n\t"\
+         "  #sti	                              \n\t"\
          "                                   \n\t"\
          "  movl $0, %ebp                    \n\t"\
          "  pushl %gs:0                      \n\t"\
          "  pushl %gs:4                      \n\t"\
          "  call core_loop                   \n\t"\
-         "                                   \n\t"\
-         :										  \
-         :										  \
-         )                                        \
+         );                                        \
 
 
 
